2770d8a7982e904ea4ffa0b91710e9ec9f38ad59
Aguimar Filho <aguimarfilho2308@gmail.com>
2024-10-09 18:15:43 -0300

    Sistema de vendas v0.0.3
    Desenvolvimento do código que pede para o usuário escolher os alimentos que serão comprados, parte 1.

--- SalesOperationsSystemController.c
+++ SalesOperationsSystemController.c
@@ -14,37 +14,41 @@ struct foods
 
 int main(void)
 {
-    setlocale(LC_ALL, "Portuguese");
+    setlocale(LC_ALL, "pt_BR.UTF-8");
     
-    // Lista de alimentos com preços e pesos definidos
-    struct foods foods[22];
+    #pragma region FoodList
+        // Lista de alimentos com preços e pesos definidos
+        struct foods foods[22];
 
-    // FRUTAS
-    // Abacates
-    strcpy(foods[0].name, "Abacates");
-    foods[0].kilo = 1; 
-    foods[0].priceUnit = 0;
-    foods[0].priceKilo = 10;
+        // FRUTAS
+        // Abacates
+        strcpy(foods[0].name, "Abacates");
+        foods[0].kilo = 1; 
+        foods[0].priceUnit = 0;
+        foods[0].priceKilo = 10;
 
-    // Abacaxis
-    strcpy(foods[1].name, "Abacaxis");
-    foods[1].kilo = 1; 
-    foods[1].priceUnit = 5;
-    foods[1].priceKilo = 6;
+        // Abacaxis
+        strcpy(foods[1].name, "Abacaxis");
+        foods[1].kilo = 1; 
+        foods[1].priceUnit = 5;
+        foods[1].priceKilo = 6;
+    #pragma endregion
     
-    //int optionBuy = 0;
-    //int i = 0, 
-    int foodCode = 0;
+    int i, foodCode;
 
-    char buyAgain = 'n';
+    char buyAgain = 's';
 
     puts("Loja de Hortifruti\n");
+    sleep(1);
     puts("Lista de alimentos disponíveis para comprar:\n");
 
+    // Mostrar lista de alimentos com preços usando matriz
+
+    // Faz a escolha dos alimentos que serão comprados e calcula o subtotal de cada alimento
     do
     {
-        buyAgain = 'n';
-        printf("Digite o código do alimento:\n");
+        sleep(1);
+        printf("Digite o código do alimento de 0 a 22:\n");
         scanf("%d", &foodCode);
 
         switch (foodCode)
@@ -55,21 +59,21 @@ int main(void)
 
             foods[0].totalPrice = foods[0].kilo * foods[0].priceKilo;
 
+            fflush(stdin);
             printf("Digite 's' para continuar comprando e 'n' para finalizar as compras:\n");
-            scanf("%c", &buyAgain);
+            scanf(" %c", &buyAgain);
             break;
         
-        default: printf("Código digitado inválido!");
-            break;
+        default: printf("Código digitado inválido!\n");
         }
-        
-    } while (buyAgain != 'n');
-    
-    // calcular total de subtotal das compras com for
+    } while (buyAgain == 's');
 
-    // calcular total das compras
+    // Calcular total das compras através da soma dos subtotais (priceTotal) de cada alimento, utilizando for
 
-    // exibir total das compras na tela
+    // Exibir total das compras na tela
 
+    sleep(1);
+    puts("Programa concluído!");
+    system("pause");
     return 0;
 }
